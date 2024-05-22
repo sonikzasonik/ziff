62f2207aebdf27e3014d78443af4e1b8fada02af
Sonya Sypalo <sonyaflake.sf@gmail.com>
2024-05-22 23:13:14 +0300

    footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,17 @@
-// const burger = document.querySelector(".header__burger");
-// const menu = document.querySelector(".header__menu");
-// const menuLinks = document.querySelectorAll(".header__link");
-
-// burger.addEventListener("click", () => {
-//   menu.classList.toggle("active");
-//   burger.classList.toggle("active");
-// });
-
-// menuLinks.forEach((link) =>
-//   link.addEventListener("click", () => {
-//     menu.classList.remove("active");
-//     burger.classList.remove("active");
-//   })
-// );
-
-// loadHeader.js
 document.addEventListener("DOMContentLoaded", function() {
+  // Load header
   fetch("/elements/header.html")
       .then(response => response.text())
       .then(data => {
           document.querySelector("header").innerHTML = data;
           initializeBurgerMenu();
+      });
+
+  // Load footer
+  fetch("/elements/footer.html")
+      .then(response => response.text())
+      .then(data => {
+          document.querySelector("footer").innerHTML = data;
       });
 });
 
@@ -41,3 +32,4 @@ function initializeBurgerMenu() {
       })
   );
 }
+
