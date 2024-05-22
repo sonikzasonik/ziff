0081bc94e58ef6d31b9b149fce43d6d8af74673f
Sonya Sypalo <sonyaflake.sf@gmail.com>
2024-05-22 23:03:45 +0300

    header

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,43 @@
-const burger = document.querySelector(".header__burger");
-const menu = document.querySelector(".header__menu");
-const menuLinks = document.querySelectorAll(".header__link");
+// const burger = document.querySelector(".header__burger");
+// const menu = document.querySelector(".header__menu");
+// const menuLinks = document.querySelectorAll(".header__link");
 
-burger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  burger.classList.toggle("active");
+// burger.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+//   burger.classList.toggle("active");
+// });
+
+// menuLinks.forEach((link) =>
+//   link.addEventListener("click", () => {
+//     menu.classList.remove("active");
+//     burger.classList.remove("active");
+//   })
+// );
+
+// loadHeader.js
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("/elements/header.html")
+      .then(response => response.text())
+      .then(data => {
+          document.querySelector("header").innerHTML = data;
+          initializeBurgerMenu();
+      });
 });
 
-menuLinks.forEach((link) =>
-  link.addEventListener("click", () => {
-    menu.classList.remove("active");
-    burger.classList.remove("active");
-  })
-);
+function initializeBurgerMenu() {
+  const burger = document.querySelector(".header__burger");
+  const menu = document.querySelector(".header__menu");
+  const menuLinks = document.querySelectorAll(".header__list-item a");
+
+  burger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+      burger.classList.toggle("active");
+  });
+
+  menuLinks.forEach((link) =>
+      link.addEventListener("click", () => {
+          menu.classList.remove("active");
+          burger.classList.remove("active");
+      })
+  );
+}
